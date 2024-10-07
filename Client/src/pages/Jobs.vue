e2cedaf63e2ac50a1cfda0f613be5c9ff24f54e2
latiwagner <latiphyver.b@gmail.com>
2024-10-07 15:11:42 -0400

    file based routing

---
+++ Client/src/pages/Jobs.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1 class="title">Jobs</h1>
+    <p>Jobs Page</p>
+  </main>
+</template>
--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,17 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import VueRouter from 'unplugin-vue-router/vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  plugins: [VueRouter({}), vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
