855453417082b90c41eb42d36cce2a21ca92e439
latiwagner <latiphyver.b@gmail.com>
2024-10-10 19:02:45 -0400

    capitalism. shop added

--- Client/src/components/ProductCard.vue
+++ Client/src/components/ProductCard.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
 import type { Product } from '@/models/products'
 
-const props = defineProps<[product: Product]>()
+defineProps<{ product: Product }>()
 </script>
 
 <template>
   <div class="box">
     <div class="box-image">
-      <img :src="Product.image" alt="product.name" />
+      <img :src="product.thumbnail" alt="product.name" />
     </div>
     <div class="box-content">
-      <h3>{{ Product.title }}</h3>
-      <p>{{ Product.description }}</p>
-      <p>{{ Product.price }}</p>
+      <h3>{{ product.title }}</h3>
+      <p>{{ product.description }}</p>
+      <p>{{ product.price }}</p>
     </div>
   </div>
 </template>
--- Client/src/models/products.ts
+++ Client/src/models/products.ts
@@ -7,3 +7,32 @@ export function getAll(): DataListEnvelope<Product> {
     total: data.total
   }
 }
+
+interface Review {
+  rating: number
+  comment: string
+  date: string
+  reviewerName: string
+  reviewerEmail: string
+}
+export interface Product {
+  id: number
+  title: string
+  description: string
+  category: string
+  price: number
+  rating: number
+  tags: string[]
+  brand?: string
+  weight: number
+  dimensions: {
+    width: number
+    height: number
+    depth: number
+  }
+  reviews: Review[]
+  returnPolicy: string
+  minimumOrderQuantity: number
+  images: string[]
+  thumbnail: string
+}
