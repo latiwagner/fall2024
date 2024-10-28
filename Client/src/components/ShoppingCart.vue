0abd584e806fa71c2081ad61f3af748cdb9d9643
latiwagner <latiphyver.b@gmail.com>
2024-10-28 14:44:46 -0400

    fixed rounding, improved cart appearance, can remove items

--- Client/src/components/ShoppingCart.vue
+++ Client/src/components/ShoppingCart.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { refCart, count, total } from '@/models/shoppingCart'
+import { refCart, count, total, removeFromCart } from '@/models/shoppingCart'
 
 const cart = refCart()
 </script>
@@ -11,16 +11,38 @@ const cart = refCart()
       <p>Your cart is empty</p>
     </div>
     <div v-else>
-      <div v-for="item in cart" :key="item.product.id">
+      <div v-for="item in cart" :key="item.product.id" class="item">
+        <img :src="item.product.thumbnail" :alt="item.product.title" />
+        <button class="button" @click="removeFromCart(item.product)" style="float: right">
+          <i class="fa-trash fas has-text-danger"></i>
+        </button>
         <div>{{ item.product.title }}</div>
-        <div>{{ item.product.price }}</div>
-        <div>{{ item.quantity }}</div>
+        <div>
+          {{ item.product.price }} * {{ item.quantity }} = ${{
+            (item.product.price * item.quantity).toFixed(2)
+          }}
+        </div>
       </div>
       <div>
-        <p>Total: {{ count }} items = ${{ total }}</p>
+        <h3>Total: {{ count }} items = ${{ total.toFixed(2) }}</h3>
       </div>
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.cart {
+  padding: 1rem;
+  overflow-y: auto;
+}
+.cart img {
+  width: 50px;
+  height: 50px;
+  float: left;
+}
+.item {
+  padding-bottom: 0.5em;
+  margin-bottom: 0.5em;
+  border-bottom: 1px ridge #ccc;
+}
+</style>
